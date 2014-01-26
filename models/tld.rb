336b2b5181c6e0140bb30dc751f43eccf9e28709
Francesco Canessa <makevoid@gmail.com>
2014-01-26 20:25:15 +0100

    temporarely remove org tld

--- models/tld.rb
+++ models/tld.rb
@@ -1,11 +1,12 @@
 class Tld
-  
+
   # TLDS = ["com", "net", "org", "it", "co.uk"]
-  TLDS = ["com", "net", "org", "it"]
+  #TLDS = ["com", "net", "org", "it"]
+  TLDS = ["com", "net", "it"] # TODO: check org for parse error
   # TLDS = ["com", "it"]
-  
+
   def self.all
     TLDS
   end
-  
+
 end
